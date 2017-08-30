55c7c1c5444a17a966377f5a3346c2586afab998
Robby <jyant90@gmail.com>
2017-08-29 22:25:56 -0500

    Initial Development

---
+++ application/views/layout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robby
+ * Date: 8/29/2017
+ * Time: 10:24 PM
+ */
--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-<h1>hull;o</h1>
+<?php
+require_once('../application/config/connection.php');
+
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action     = $_GET['action'];
+} else {
+    $controller = 'pages';
+    $action     = 'home';
+}
+
+require_once('../application/views/layout.php');
+?>
